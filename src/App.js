00fdab379e2851400c9017cf7019bb602ceb8469
Paul Silva <paulofs86@gmail.com>
2023-10-19 16:37:12 -0500

    Hamburger menu added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react';
+
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Paulo Silva portfolio site.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav" id="desktop-nav">
+        <div className="logo">Paulo Silva</div>
+        <div className="Button-container">
+          <div className="Button">Portfolio</div>
+          <div className="Button">About</div>
+          <div className="Button">Contact</div>
+        </div>
+      </div>
+      <div className="hamburger-nav">
+        <div className="logo">Paulo Silva</div>
+        {menuOpen ? null : <div className="hamburger-menu" onClick={toggleMenu}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div></div>}
+            {menuOpen ? <div className="menu-links">
+                <div className="x" onClick={toggleMenu}>X</div>
+                <div>Portfolio</div>
+                <div>About</div>
+                <div>Contact</div>
+               </div> : null}
+      </div>
     </div>
   );
 }
